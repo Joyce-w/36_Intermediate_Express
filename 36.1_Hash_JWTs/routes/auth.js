0ff24ffb9ada6c68544a754f31fa0605c34bc543
Joyce <hellojoycewong@gmail.com>
2021-04-29 20:45:51 -0700

    write register route

--- 36.1_Hash_JWTs/routes/auth.js
+++ 36.1_Hash_JWTs/routes/auth.js
@@ -1,3 +1,8 @@
+const { Router } = require("express")
+const router = new Router();
+const { User } = require("../models/user")
+const jwt = require("jsonwebtoken")
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
@@ -11,3 +16,14 @@
  *
  *  Make sure to update their last-login!
  */
+
+Router.post("/register", async (req, res, next) => {
+    try {
+        const { username, password, first_name, last_name, phone } = req.body;
+        let user = await User.register(username, password, first_name, last_name, phone);
+        return res.json(user)        
+    } catch (e) {
+        next(e)
+    }
+
+})
