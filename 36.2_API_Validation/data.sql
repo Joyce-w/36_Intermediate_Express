d99f25892b82cc3d950b27aa94af5328833a5ee2
Joyce <hellojoycewong@gmail.com>
2021-05-06 14:32:25 -0700

    Starter code + api validation for book creation

---
+++ 36.2_API_Validation/data.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE books;
+
+\c books;
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT,
+  language TEXT, 
+  pages INTEGER,
+  publisher TEXT,
+  title TEXT, 
+  year INTEGER
+);
+
